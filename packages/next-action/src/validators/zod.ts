1d26f85a54ad2381e8a975073503e2481009da41
Freddy L <neo-ciber_94@hotmail.com>
2024-05-12 17:56:56 -0400

    Update package.json

--- packages/next-action/src/validators/zod.ts
+++ packages/next-action/src/validators/zod.ts
@@ -5,6 +5,7 @@ import type { ZodType } from "zod";
  * Convert a zod schema to a `Validator<T>`.
  * @param schema The `zod` schema.
  * @returns An schema compatible with the `Validator<T>` interface.
+ * @remarks You don't need to use this, schema is already a valid `Validator<T>`.
  */
 export default function $zod<T>(schema: ZodType<T>): Validator<T> {
     return schema;
